6fc95fee6555611b1d727aa6c65f82ffcbd08f49
Qazeer <qazeerat@gmail.com>
2013-10-10 23:29:13 +0200

    Update rotate.ml

--- rotate.ml
+++ rotate.ml
@@ -2,20 +2,15 @@
 let degreToRadian ang = 
   ang /. 180. *. 3.1415965359
 
-(* Donne la position de x dans l'image original (avant scannage) *)
+(* Donne la position de x dans l'image source (l'image telle qu'elle était avant d'etre scannée pas droit)*)
 (* x,y : coordonnés dans l'image scannée, ang : angle dont l'image est roté(du verbe francais rotationner sisi), cx,cy : centre de l'image *) 
 let initX x y ang cx cy =
   cx + truncate((float_of_int(x - cx)) *. cos(ang) -. (float_of_int(y - cy)) *. sin(ang))
 
-(* Donne la position de y dans l'image original (avant scannage) *)
+(* Donne la position de y dans l'image source *)
 (* x,y : coordonnés dans l'image scannée, ang : angle dont l'image est roté, cx,cy : centre de l'image *) 
 let initY x y ang cx cy =
  cy + truncate((float_of_int(x - cx)) *. (sin(ang)) +. (float_of_int(y - cy)) *. cos(ang))
-
-(* Test si un pixel est noire *)
-let isBlack (r,g,b) =
-  let rf = float_of_int r and gf = float_of_int g and bf = float_of_int b
-  in 0. = (0.3*.rf +. 0.59*.gf +. 0.11*.bf)/.255.
   
 (* img = image source du scanner, dst = image de destination, version pas optimisé car la version opti need une image deja blanche de depart *)
 let rotate img dst angDegre =
@@ -28,7 +23,7 @@ let rotate img dst angDegre =
       let x = initX i j ang (w/2) (h/2)
       and y = initY i j ang (w/2) (h/2)   in	
         if (x >= 0) && (y >= 0) && (x < w-1) && (y < h-1) then
-	  if (isBlack(Sdlvideo.get_pixel_color img i j)) then
+	  if Sdlvideo.get_pixel_color img i j = (0,0,0) then
 	      Sdlvideo.put_pixel_color dst x y (0,0,0)
 	  else 
                Sdlvideo.put_pixel_color dst x y (255,255,255)
