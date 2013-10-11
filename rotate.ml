ec201e07d967129a435ada7c03e0eb1a6f559a61
Qazeer <qazeerat@gmail.com>
2013-10-12 00:55:24 +0200

    Update rotate.ml

--- rotate.ml
+++ rotate.ml
@@ -15,14 +15,13 @@ let initY x y ang cx cy =
   cy + truncate((float_of_int(x - cx)) *. (sin(ang)) +. (float_of_int(y - cy)) *. cos(ang))
 
 (* Is the pixel (x,y) in bound ? *) 
-let isInBound x y img =
+let isInBound img x y =
   let (w,h) = get_dims img in 
     (x >= 0) && (y >= 0) && (x < w-1) && (y < h-1)
 
 (* Passe une image en blanc (provisoire) *)
 let toWhite img = 
   let (w,h) = get_dims img in
-
   for i = 0 to w - 1 do
     for j = 0 to h - 1 do
       Sdlvideo.put_pixel_color img i j (255,255,255)
@@ -34,14 +33,12 @@ let toWhite img =
 let rotate img dst angDegre =
   let (w,h) = get_dims img 
   and ang = degreToRadian angDegre in
-  
   for i = 0 to w-1 do
     for j = 0 to h-1 do
       if Sdlvideo.get_pixel_color img i j = (0,0,0) then
 	let x = initX i j ang ((w-1)/2) ((h-1)/2)
-	and y = initY i j ang ((w-1)/2) ((h-1)/2) in	
-
-        if isInBound x y img then
+	and y = initY i j ang ((w-1)/2) ((h-1)/2) in
+        if isInBound img x y then
 	  Sdlvideo.put_pixel_color dst x y (0,0,0)
     done
   done
