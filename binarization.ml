479710c2274f54cd090af2fc9c56486ac70076d7
Qazeer <qazeerat@gmail.com>
2013-10-19 00:25:05 +0200

    Otsu method 1.0
    Binarization using the Otsu method

--- binarization.ml
+++ binarization.ml
@@ -33,7 +33,7 @@ let image2bnw src dst =
     done 
   done
 
-
+(* binarization using average level of img's pixel as treshold *)
 let binarization src dst = 
   let (w,h) = Rotate.get_dims src in
   let greyImage = Sdlvideo.create_RGB_surface_format src [] w h in
@@ -55,5 +55,96 @@ let binarization src dst =
 	Sdlvideo.put_pixel_color dst j k (0,0,0)
       else 
 	Sdlvideo.put_pixel_color dst j k (255,255,255)
+    done
+  done
+
+(* ------------------ Otsu stuff ------------------ *)
+
+(* histo *) 
+let getHistogramme img = 
+  begin
+    let (w,h) = Function.get_dims img
+    and histo = Array.init 256 (fun x -> 0) in
+    for i = 0 to w-1 do
+      for j = 0 to h-1 do
+	let greyColor = int_of_float (Function.level (Sdlvideo.get_pixel_color img i j) *. 255.) in
+	histo.(greyColor) <- histo.(greyColor) + 1;
+      done;
+    done;
+    histo
+  end
+
+(* The histogramme will contains the probability of occurrence of level in the image *)
+let equalizationHisto histo n = 
+  for i = 0 to 256 do
+    histo.(i) <- histo.(i) / n
+  done
+
+(* Calcule the within class variance for a given pixel in a equalized histogramme *)
+let variance histo tresh numPixel =
+  begin
+    (* Background shit *)
+    let bNumPixel = ref 0.
+    and bWeight = ref  0.
+    and bMean = ref 0.
+    and bVariance = ref 0. in  
+    for i = 0 to tresh do 
+      bWeight := !bWeight +. float_of_int histo.(i);
+      bMean := !bMean +. float_of_int (i * histo.(i));
+      bNumPixel := !bNumPixel +. 1.;
+    done;
+    bWeight := !bWeight /. float_of_int numPixel;
+    bMean := !bMean /. !bNumPixel;
+    for i = 0 to tresh do
+      bVariance := !bVariance +. ((((float_of_int i) -. !bMean)**2.) *. (float_of_int histo.(i)));
+    done;
+    bVariance := !bVariance /. !bNumPixel;
+    (* Foreground shit *)
+    let fNumPixel = ref 0.
+    and fWeight = ref  0.
+    and fMean = ref 0.
+    and fVariance = ref 0. in  
+    for i = tresh+1 to 256 do 
+      fWeight := !fWeight +. float_of_int histo.(i);
+      fMean := !fMean +. float_of_int (i * histo.(i));
+      fNumPixel := !fNumPixel +. 1.;
+    done;
+    fWeight := !fWeight /. float_of_int numPixel;
+    fMean := !fMean /. !fNumPixel;
+    for i = tresh+1 to 256 do
+      fVariance := !fVariance +. ((((float_of_int i) -. !fMean)**2.) *. (float_of_int histo.(i)));
+    done;
+    fVariance := !fVariance /. !fNumPixel;
+    let intraClasseVariance  = !bWeight *. !bVariance +. !fWeight *. !fVariance in
+    intraClasseVariance;
+    end	
+
+let minVariance histo numPixel = 
+  begin
+    let minVar = ref 0. 
+    and treshold = ref 0 in
+    for i = 0 to 256 do
+      let var = variance histo i numPixel in
+      if var < !minVar then
+	minVar := var;
+        treshold := i;      
+    done;
+    treshold;
+    end
+
+(* binarization using Otsu's method, src must be greyscaled *)
+let binarizationOtsu src dst = 
+  let (w,h) = Function.get_dims src 
+  and histo = getHistogramme src in
+  let numPixel = w * h in
+  let histo = equalizationHisto numPixel in
+  let treshold = minVariance histo numPixel in
+  for i = 0 to w - 1 do
+    for j = 0 to h - 1 do
+      let (color,_,_) = Sdlvideo.get_pixel_color src i j in
+      if color <= treshold then
+	Sdlvideo.put_pixel_color dst i j (0,0,0)
+      else
+	Sdlvideo.put_pixel_color dst i j (255,255,255)
     done
   done
