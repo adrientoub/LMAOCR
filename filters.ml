156e673b2bcf91179b0b583708d897e073bbd091
Qazeer <qazeerat@gmail.com>
2013-10-19 18:54:07 +0200

    Update filters.ml

--- filters.ml
+++ filters.ml
@@ -249,10 +249,9 @@ let filterMedianColor img x y =
 let passeHaut = [| [|0,-1,0|], [|-1,5,-1|], [|0,-1,0|] |];;
 
 let applyLinearFilter img dst tabCoeff = 
-  let imgMatrix = t_matrix.imgToMatrix img in
-  let imgDst = t_matrix.multMatrix imgMatrix tabCoeff
-  in t_matrix.matrixToImage imgDst 
-  
+  let imgMatrix = T_matrix.imgToMatrix img in
+  let dstMatrix = T_matrix.multMatrix imgMatrix tabCoeff
+  in T_matrix.matrixToImg dstMatrix dst 
     
   
   
