93e7ca9a10db7ab4231dddffdfa4f6cf04535327
Damien Pradier <chevalier203@hotmail.fr>
2013-12-03 00:11:03 +0100

    fonctionnel a 100% sur rot5

--- Src/extract.ml
+++ Src/extract.ml
@@ -1,5 +1,3 @@
-let scanned2 = ref (Array.create_matrix 1 1 false) (* Tableau qui contient les pixels scannés *)
-
 (* Tests if a line only has white pixels *)
 let test_empty_line img h w0 wmax =
   let rec f img h w  =
@@ -35,45 +33,38 @@ let set_grey_column img w h0 hmax =
   done
 
 (* Detects the empty lines and replace them by grey color *)
-let trace_lines img w0 wmax =
-  let (_,height) = Function.get_dims img in
-  for h = 0 to height - 1 do
+let trace_lines img w0 wmax h0 hmax =
+  for h = h0 to hmax do
     if test_empty_line img h w0 wmax then
       set_grey_line img h w0 wmax
   done
     
 (* Detects the empty columns and replace them by grey color *)    
-let trace_lines_column img h0 hmax =
-  let (width, _) = Function.get_dims img in
-  for w = 0 to width - 1 do
+let trace_lines_column img h0 hmax w0 wmax =
+  for w = w0 to wmax do
     if test_empty_column img w h0 hmax then
       set_grey_column img w h0 hmax
   done
 
 let findWhite img = 
-  let (width, height) = Function.get_dims img and i = ref 0 and j = ref 0 and compteur = ref 0 and nbitemax = 30 in
-  while !j < height && !compteur < nbitemax do
-    while !i < width && !compteur < nbitemax do
+  let (width, height) = Function.get_dims img and i = ref 0 and j = ref 0 and compteur = ref 0 and nbitemax = 4 in
+  while !j < height do
+    while !i < width do
       let (r,g,b) = Sdlvideo.get_pixel_color img !i !j in
       if (r = 255 && g = 255 && b = 255) then
 	begin
-	  (* On determine la largeur et la longueur du bloc *)
+	  (* On determine la largeur et la longueur du bloc de texte *)
 	  let x = ref !i and y = ref !j in
-	  while (!x < width && Sdlvideo.get_pixel_color img !x !j = (255,255,255)) do
+	  while (!x < width && (Sdlvideo.get_pixel_color img !x !j != (255,255,255) || Sdlvideo.get_pixel_color img !x !j = (0,0,0))) do
 	    x := !x + 1;
 	  done;
-	  while (!y < height && Sdlvideo.get_pixel_color img !i !y = (255,255,255)) do
+	  while (!y < height && (Sdlvideo.get_pixel_color img !i !y = (255,255,255) || Sdlvideo.get_pixel_color img !i !y = (0,0,0))) do
 	    y := !y + 1;
 	  done;
-	  (*for ni = !i to (!x-1) do
-	    for nj = !j to (!y-1) do
-	      !scanned2.(!i).(!j) <- true;
-	      Printf.printf "Cherjiojiojition...\n%!";
-	    done;
-	  done;*)
-	  trace_lines img !i !x;
-	  trace_lines_column img !j !y;
-	  (* Printf.printf "Position : %i %i w,h : %i %i\n" !i !j !x !y; *)
+	  (* On separe les caractères dans ce bloc *)
+	  trace_lines img !i (!x - 1) !j (!y - 1);
+	  trace_lines_column img !j (!y - 1) !i (!x - 1);
+	  (*Printf.printf "Position : %i %i w,h : %i %i\n" !i !j !x !y;*)
 	  i := !x;
 	  j := !y;
 	  compteur := !compteur + 1;
@@ -86,7 +77,6 @@ let findWhite img =
 
 let charDetection img = 
     let (width, height) = Function.get_dims img in
-    trace_lines img 0 (width-1);
-    trace_lines_column img 0 (height-1);
-    scanned2 := Array.create_matrix width height false;
+    trace_lines img 0 (width-1) 0 (height-1);
+    trace_lines_column img 0 (height-1) 0 (width - 1);
     findWhite img
